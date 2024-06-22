2b829ee9f5562e1d81e54c576800b85066115bf3
Zhen <HI20151215@GMAIL.COM>
2024-06-21 23:44:50 -0400

    random value for mem

--- server/aserver/main.go
+++ server/aserver/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 	"net"
 	"os"
 	"time"
@@ -34,14 +35,18 @@ func (s *Server) SayHello(req *pb.SayHelloRequest, stream pb.SayHelloService_Say
 	// 	return errors.New("invalid token")
 	// }
 
-	timer := time.NewTicker(time.Second * 2)
+	timer := time.NewTicker(time.Millisecond * 3000)
 	for {
 		select {
 		case <-stream.Context().Done():
 			fmt.Println("Client disconnected")
 			return nil
 		case <-timer.C:
-			err := stream.Send(&pb.SayHelloResponse{ResponseMsg: "Hello there"})
+			rand.Seed(time.Now().UnixNano())
+			randomFloat := rand.Float64() * 0.99
+			responseMsg := fmt.Sprintf("%.2f", randomFloat)
+			fmt.Printf("send message: %s\n", responseMsg)
+			err := stream.Send(&pb.SayHelloResponse{ResponseMsg: responseMsg})
 			if err != nil {
 				return nil
 			}
@@ -53,7 +58,6 @@ func main() {
 	path, _ := os.Getwd()
 	creds, _ := credentials.NewServerTLSFromFile(path+"/keys/server/server.pem", path+"/keys/server/server.key")
 	grpcServer := grpc.NewServer(grpc.Creds(creds))
-	//grpcServer := grpc.NewServer(grpc.Creds(insecure.NewCredentials()))
 	pb.RegisterSayHelloServiceServer(grpcServer, &Server{})
 	err := grpcServer.Serve(listen)
 	if err != nil {
